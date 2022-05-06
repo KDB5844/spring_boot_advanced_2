e5dc2eccc3e0206f28cbc83a61eaf675a0515838
Subin Cho <sbjo7713@gmail.com>
2022-05-07 06:04:39 +0900

    테스트 코드 리펙토링

--- src/main/java/hello/proxy/config/v3_proxyfactory/ProxyFactoryConfigV1.java
+++ src/main/java/hello/proxy/config/v3_proxyfactory/ProxyFactoryConfigV1.java
@@ -5,9 +5,11 @@ import hello.proxy.config.v3_proxyfactory.advice.LogTraceAdvice;
 import hello.proxy.trace.logtrace.LogTrace;
 import hello.proxy.trace.logtrace.ThreadLocalLogTrace;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.aop.Advisor;
 import org.springframework.aop.Pointcut;
 import org.springframework.aop.framework.ProxyFactory;
 import org.springframework.aop.support.DefaultPointcutAdvisor;
+import org.springframework.aop.support.NameMatchMethodPointcut;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -16,30 +18,36 @@ import org.springframework.context.annotation.Configuration;
 public class ProxyFactoryConfigV1 {
 
     @Bean
-    public OrderControllerV1 orderControllerV1(LogTrace trace) {
-        DefaultPointcutAdvisor advisor = new DefaultPointcutAdvisor(Pointcut.TRUE, new LogTraceAdvice(trace));
-        ProxyFactory proxyFactory = new ProxyFactory(new OrderControllerV1Impl(orderServiceV1(trace)));
-        proxyFactory.addAdvisor(advisor);
+    public OrderControllerV1 orderControllerV1(LogTrace logTrace) {
+        ProxyFactory proxyFactory = new ProxyFactory(new OrderControllerV1Impl(orderServiceV1(logTrace)));
+        proxyFactory.addAdvisor(getAdvisor(logTrace));
         OrderControllerV1 proxy = (OrderControllerV1) proxyFactory.getProxy();
         return proxy;
     }
 
     @Bean
-    public OrderServiceV1 orderServiceV1(LogTrace trace) {
-        DefaultPointcutAdvisor advisor = new DefaultPointcutAdvisor(Pointcut.TRUE, new LogTraceAdvice(trace));
-        ProxyFactory proxyFactory = new ProxyFactory(new OrderServiceImpl(orderRepositoryV1(trace)));
-        proxyFactory.addAdvisor(advisor);
+    public OrderServiceV1 orderServiceV1(LogTrace logTrace) {
+        ProxyFactory proxyFactory = new ProxyFactory(new OrderServiceImpl(orderRepositoryV1(logTrace)));
+        proxyFactory.addAdvisor(getAdvisor(logTrace));
         OrderServiceV1 proxy = (OrderServiceV1) proxyFactory.getProxy();
         return proxy;
     }
 
     @Bean
     public OrderRepositoryV1 orderRepositoryV1(LogTrace logTrace) {
-        DefaultPointcutAdvisor advice = new DefaultPointcutAdvisor(Pointcut.TRUE, new LogTraceAdvice(logTrace));
         ProxyFactory proxyFactory = new ProxyFactory(new OrderRepositoryV1Impl());
-        proxyFactory.addAdvisor(advice);
+        proxyFactory.addAdvisor(getAdvisor(logTrace));
         OrderRepositoryV1 proxy = (OrderRepositoryV1) proxyFactory.getProxy();
         return proxy;
+    }
+
+    private Advisor getAdvisor(LogTrace logTrace) {
+        // pointcut
+        NameMatchMethodPointcut pointcut = new NameMatchMethodPointcut();
+        pointcut.setMappedNames("request*", "order*", "save*");
+        // advisor
+        LogTraceAdvice advice = new LogTraceAdvice(logTrace);
+        return new DefaultPointcutAdvisor(pointcut, advice);
     }
 
 }
